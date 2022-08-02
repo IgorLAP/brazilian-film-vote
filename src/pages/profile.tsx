0f97e0cb212ec4cf0139daf596bc4f8ec52c91ee
Igor Pedrosa <ilap1036@gmail.com>
2022-08-02 11:43:47 -0300

    refactor: provide cookies and redirects user based on their roles

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,5 +1,28 @@
 import React from "react";
 
+import { GetServerSideProps } from "next";
+
+import { firebaseAdmin } from "~/lib/firebase-admin";
+
 export default function Profile() {
   return <h1>Profile</h1>;
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  const verify = await firebaseAdmin.auth().verifyIdToken(token);
+
+  return {
+    props: {},
+  };
+};
