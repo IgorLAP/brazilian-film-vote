716a1d2bdb240c6843f20f71aba42100c538e51f
Igor Pedrosa <ilap1036@gmail.com>
2022-08-03 18:55:24 -0300

    feat: signin page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -49,73 +49,75 @@ export function Header() {
         </Text>
         azilian film vote
       </Text>
-      <Flex>
-        <Flex
-          fontSize="small"
-          color="gray.400"
-          flexDir="column"
-          justify="center"
-          align="flex-end"
-          mr="2"
-        >
-          <Text>{loggedUser?.displayName || ""}</Text>
-          <Text>{loggedUser?.email}</Text>
-        </Flex>
-        <Popover>
-          <PopoverTrigger>
-            <Button variant="unstyled">
-              <Image
-                h="48px"
-                w="48px"
-                objectFit="cover"
-                objectPosition="center"
-                borderRadius={10}
-                border="2px"
-                bg="gray.100"
-                borderColor="blue.400"
-                src={
-                  loggedUser?.photoURL ||
-                  "https://icon-library.com/images/white-profile-icon/white-profile-icon-24.jpg"
-                }
-              />
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent w="28" mr="6" mt="1">
-            <PopoverBody
-              display="flex"
-              justifyContent="center"
-              alignItems="center"
-              flexDir="column"
-            >
-              <Flex
-                _hover={{ color: "blue.400" }}
-                w="100%"
-                justify="flex-start"
-                align="center"
-              >
-                <Icon mr="2" as={BsFillPersonFill} />
-                <ChakraLink as={NextLink} href="#">
-                  Profile
-                </ChakraLink>
-              </Flex>
-
-              <Divider />
-              <Button
-                display="flex"
-                justifyContent="flex-start"
-                alignItems="center"
-                fontWeight="normal"
-                onClick={signOut}
-                variant="unstyled"
-                _hover={{ color: "blue.400" }}
-              >
-                <Icon mr="2" as={GoSignOut} />
-                Sign out
+      {loggedUser && (
+        <Flex>
+          <Flex
+            fontSize="small"
+            color="gray.400"
+            flexDir="column"
+            justify="center"
+            align="flex-end"
+            mr="2"
+          >
+            <Text>{loggedUser?.displayName || ""}</Text>
+            <Text>{loggedUser?.email}</Text>
+          </Flex>
+          <Popover>
+            <PopoverTrigger>
+              <Button variant="unstyled">
+                <Image
+                  h="48px"
+                  w="48px"
+                  objectFit="cover"
+                  objectPosition="center"
+                  borderRadius={10}
+                  border="2px"
+                  bg="gray.100"
+                  borderColor="blue.400"
+                  src={
+                    loggedUser?.photoURL ||
+                    "https://icon-library.com/images/white-profile-icon/white-profile-icon-24.jpg"
+                  }
+                />
               </Button>
-            </PopoverBody>
-          </PopoverContent>
-        </Popover>
-      </Flex>
+            </PopoverTrigger>
+            <PopoverContent w="28" mr="6" mt="1">
+              <PopoverBody
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                flexDir="column"
+              >
+                <Flex
+                  _hover={{ color: "blue.400" }}
+                  w="100%"
+                  justify="flex-start"
+                  align="center"
+                >
+                  <Icon mr="2" as={BsFillPersonFill} />
+                  <ChakraLink as={NextLink} href="#">
+                    Profile
+                  </ChakraLink>
+                </Flex>
+
+                <Divider />
+                <Button
+                  display="flex"
+                  justifyContent="flex-start"
+                  alignItems="center"
+                  fontWeight="normal"
+                  onClick={signOut}
+                  variant="unstyled"
+                  _hover={{ color: "blue.400" }}
+                >
+                  <Icon mr="2" as={GoSignOut} />
+                  Sign out
+                </Button>
+              </PopoverBody>
+            </PopoverContent>
+          </Popover>
+        </Flex>
+      )}
     </Flex>
   );
 }
