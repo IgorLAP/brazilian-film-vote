47c77c28ef6080d394773b5ff93888b6094c5111
Igor Pedrosa <ilap1036@gmail.com>
2022-08-03 18:52:40 -0300

    refactor: proper redirects if logged user

--- src/helpers/veritySSRAuth.ts
+++ src/helpers/veritySSRAuth.ts
@@ -4,19 +4,46 @@ import {
   GetServerSidePropsResult,
 } from "next";
 
+import { firebaseAdmin } from "~/lib/firebase-admin";
+
 export function verifySSRAuth(fn: GetServerSideProps) {
   return async (
     ctx: GetServerSidePropsContext
   ): Promise<GetServerSidePropsResult<unknown>> => {
     const { token } = ctx.req.cookies;
+    const { resolvedUrl } = ctx;
 
-    if (!token) {
+    if (!token && resolvedUrl !== "/") {
       return {
         redirect: {
           destination: "/",
           permanent: false,
         },
       };
+    }
+
+    if (token) {
+      const auth = firebaseAdmin.auth();
+      const { uid } = await auth.verifyIdToken(token);
+      const { customClaims } = await auth.getUser(uid);
+
+      if (customClaims?.admin && resolvedUrl !== "/admin") {
+        return {
+          redirect: {
+            destination: "/admin",
+            permanent: false,
+          },
+        };
+      }
+
+      if (!customClaims?.admin && resolvedUrl !== "/profile") {
+        return {
+          redirect: {
+            destination: "/profile",
+            permanent: false,
+          },
+        };
+      }
     }
 
     return fn(ctx);
