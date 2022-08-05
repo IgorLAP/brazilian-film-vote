2cd923ed1b4febf5632813f63429b2dce7e7df59
Igor Pedrosa <ilap1036@gmail.com>
2022-08-05 09:41:48 -0300

    refactor: layout component provide header and siderbar

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -2,16 +2,10 @@ import React from "react";
 
 import { GetServerSideProps } from "next";
 
-import { Header } from "~/components/Header";
 import { verifySSRAuth } from "~/helpers/veritySSRAuth";
 
 export default function Profile() {
-  return (
-    <>
-      <Header />
-      <h1>Profile</h1>
-    </>
-  );
+  return <h1>Profile</h1>;
 }
 
 export const getServerSideProps: GetServerSideProps = verifySSRAuth(
